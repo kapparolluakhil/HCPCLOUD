9a31b665113c4853500176081f5cb57f527564b3
kapparolluakhil <125285191+kapparolluakhil@users.noreply.github.com>
2026-06-19 00:20:16 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+# ---------------- EC2 ----------------
+resource "aws_instance" "one" {
+  ami                    = "ami-0199ac7c9fbf9ed83"
+  instance_type          = "t3.micro"
+  tags = {
+    Name = "Ec2-instance"
+  }
+}
